9b8f205e9957ec9744db3e8e2a84e9b371a639e2
Lucas Saavedra <lucassaavedra50@gmail.com>
2021-08-13 20:07:38 -0300

    Script del carrito

--- js/app.js
+++ js/app.js
@@ -1,57 +1,103 @@
+/* clase producto */
 class Producto {
-    constructor (nombre,precio,stock,categoria,color){
-        this.nombre=nombre;
-        this.precio=parseFloat(precio);
-        this.stock=Number(stock);
-        this.categoria=categoria;
-        this.color=color;
+  constructor(id, nombre, precio, stock, categoria, color) {
+    this.id = Number(id);
+    this.nombre = nombre.toLowerCase();
+    this.precio = parseFloat(precio);
+    this.stock = Number(stock);
+    this.categoria = categoria;
+    this.color = color;
+  }
+  /* metodos */
+  cambiarNombre(nombre) {
+    nombre = prompt('Ingrese el nuevo nombre');
+    alert(`Nombre viejo: ${this.nombre}, nuevo nombre ${nombre}`)
+    this.nombre = nombre;
+  }
+  hayStock() {
+    return this.stock > 0 ? true : false;
+  }
+  comprarProducto() {
+    if (this.hayStock(this.stock)) {
+      this.stock--;
+      alert(`Felicitaciones! Compraste ${this.nombre} a $${this.precio},restantes ${this.stock}`);
+      return true;
+    } else {
+      alert('No se puede comprar, no hay stock');
+      return false;
     }
-    cambiarNombre(nombre){
-        this.nombre=nombre
-    }
-    consultarStock(){
-        if (this.stock > 0) return true;
-    }
+  }
 }
+/* seteo de variables */
+salida = false;
+carrito = [];
+
+/* instancio los productos */
+producto1 = new Producto(1, 'Lampara', 3000, 0, 'iluminacion', 'blanco');
+producto2 = new Producto(2, 'Ventilador', 5000, 10, 'acondicionamiento', 'negro');
+producto3 = new Producto(3, 'Sillon', 7000, 3, 'muebles', 'gris');
+producto4 = new Producto(4, 'Mesa de luz', 3000, 0, 'iluminacion', 'marron');
+producto5 = new Producto(4, 'Pelota', 1000, 3, 'futbol', 'marron');
+
+/* funciones para trabajar el carrito */
+const agregarCarrito = (producto) => {
+  if (producto.comprarProducto()) carrito.push(producto);
+}
+
+const busquedaId = (array, id) => {
+  return array.find(e => e.id == Number(id));
+}
+
+const filtroCat = (array, categoria) => {
+  return array.filter(e => e.categoria.includes(categoria));
+}
+/* menu interactivo de compra */
+alert('Seleccione que desea comprar o hacer')
 do {
-    switch (Number(prompt('Comprar[1] | Invertir la frase[2] | calcular las cuotas[3] | adivina el numero[4] | salir [5] '))) {
-      case 1:
-        palabra = prompt('Ingrese una palabra');
-        alert(`Su palabra cuenta con: ${contVocal(palabra)} vocales`)
-        
-        break;
-      case 2:
-        palabra = prompt('Ingrese una frase para revertirla');
-        alert(`LA frase original:${palabra}, nueva frase: ${revertFrase(palabra)}`)
-        break;
-      case 3:
-        let monto = Number(prompt('Ingrese una monto'));
-        let cantidad = Number(prompt('Ingrese la cantidad de cutotas a pagar'));
-        let interes = Number(prompt('Ingrese el interes de las cuotas'));
-        cuota = cuotas(cantidad, monto,interes);
-        console.log(cuota);
-        alert(`Debe pagar $${Math.ceil(cuota)} cada cuota`)
-        break;
-      case 4:
-        alert('Juego adivina el número')
-        do {
-          if (adivinaNum(Number(prompt('Ingrese un numero entre el 1 y 5')))) {
-            alert('Acertaste!')
-            break;
-          } else {
-            intentos--;
-            alert(`Oh no! Ese no era el numero!, cantidad de intestos restantes: ${intentos} `);
-          }
-        } while (intentos > 0);
-        break;
-      case 5:
-        alert('Gracias por usar nuetro programa');
-        salida=true;
-        break;
-      default:
-        alert = confirm('Esa opcion no existe!')
-        salida = confirmSalida();
-        break;
-    }
-  } while (salida == false);
-producto1= new Producto('');
+  switch (Number(prompt(`
+    [${producto1.id}] ${producto1.nombre} | $ ${producto1.precio}
+    [${producto2.id}] ${producto2.nombre} | $ ${producto2.precio}
+    [${producto3.id}] ${producto3.nombre} | $ ${producto3.precio}
+    [${producto4.id}] ${producto4.nombre} | $ ${producto4.precio}
+    [5] Mostrar carrito
+    [6] Filtrar carrito por categoria
+    [7] Salir
+    `))) {
+    case 1:
+      if (producto1.comprarProducto()) {
+        carrito.push(producto1);
+      }
+
+      break;
+    case 2:
+      if (producto2.comprarProducto()) {
+        carrito.push(producto2);
+      }
+
+      break;
+    case 3:
+      if (producto3.comprarProducto()) {
+        carrito.push(producto3);
+      }
+      break;
+    case 4:
+      if (producto4.comprarProducto()) {
+        carrito.push(producto4);
+      }
+      break;
+    case 5:
+      console.log(carrito);
+      break;
+    case 6:
+      carritoFiltrado = filtroCat(carrito, prompt('Escriba una categoria'));
+      console.log(carritoFiltrado);
+      break;
+    case 7:
+      alert('Gracias por usar nuetro programa');
+      salida = true;
+      break;
+    default:
+      alert = confirm('Esa opcion no existe!')
+      break;
+  }
+} while (salida == false);
