775f1ec80e3c3f396b3725ef561c576a392b19e8
Lucas Saavedra <lucassaavedra50@gmail.com>
2021-08-21 21:02:30 -0300

    Se presetearon algunos productos para hacer las pruebas

---
+++ js/productos.js
@@ -0,0 +1,59 @@
+const productos = [{
+    'id': 1,
+    'titulo': 'Lampara',
+    'precio': 3000,
+    'cat': 'iluminacion',
+    'color': 'blanco'
+  },
+  {
+    'id': 2,
+    'titulo': 'Ventilador',
+    'precio': 5000,
+    'cat': 'acondicionamiento',
+    'color': 'negro'
+  },
+  {
+    'id': 3,
+    'titulo': 'Sillon',
+    'precio': 7000,
+    'cat': 'muebles',
+    'color': 'gris'
+  },
+  {
+    'id': 4,
+    'titulo': 'Mesa de luz',
+    'precio': 3000,
+    'cat': 'iluminacion',
+    'color': 'amariilo'
+  },
+  {
+    'id': 5,
+    'titulo': 'Pelota',
+    'precio': 1000,
+    'stock': 1,
+    'cat': 'futbol',
+    'color': 'blanco'
+  },
+  {
+    "id": 6,
+    "titulo": "Anillo de oro",
+    "precio": 50000,
+    "cat": "joyeria",
+    'color': 'amarillo'
+    },
+    {
+    "id": 7,
+    "titulo": "Taladro inalambrico",
+    "precio": 10000,
+    "cat": "hogar",
+    'color': 'blanco'
+    },
+    {
+    "id": 8,
+    "titulo": "Pava electrica",
+    "precio": 1500,
+    "cat": "electrodomesticos",
+    'color': 'rojo'
+    },
+
+];
